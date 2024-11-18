715cf881d95aa3b7b9a80a5428ea28ac40e9ff4d
Nodelay <121840258+Iraildo-Sousa@users.noreply.github.com>
2024-11-18 14:49:47 -0300

    Create If_else.js
    Aqui estamos testando condicionais if e else.

---
+++ Treinamento/If_else.js
@@ -0,0 +1,67 @@
+let resposta = confirm("Você gosta de estudar?");
+
+    if (resposta) {
+
+        console.log("Então está no lugar certo!");
+    
+    }
+
+    else {
+
+        console.log("Sempre haverá um lote para capinar!");
+    }
+VM1995:5 Então está no lugar certo!
+
+
+
+
+_________________________________________________________________________________________________________
+
+let resposta = confirm("Você gosta de estudar?");
+
+    if (resposta == false) {
+
+        console.log(resposta);
+    
+    }
+
+    else {
+
+        console.log(resposta);
+    }
+VM2133:11 true
+
+__________________________________________________________________________________________________________
+
+let resposta = confirm("Você gosta de estudar?");
+
+    if (reposta != false) {
+
+        console.log("Então está no lugar certo!");
+    
+    }
+
+    else {
+
+        console.log("Sempre haverá um lote para capinar!");
+
+    }
+VM2354:11 Sempre haverá um lote para capinar!
+
+___________________________________________________________________________________________________________
+
+let maior = true, habilitado = false, empregado = true;
+
+if (maior && habilitado && empregado)
+{
+
+    console.log("Vou comprar um carro!")
+    
+}
+
+else
+{
+
+    console.log("Vou ficar maior, tirar minha carteira, estudar mais e conseguir um emprego!")
+    
+}
