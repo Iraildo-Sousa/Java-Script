d45d87978851111d8e9b147326615d9698247d00
Nodelay <121840258+Iraildo-Sousa@users.noreply.github.com>
2024-11-18 14:36:47 -0300

    Create Intro_Objetos.js
    Aqui é apenas uma breve introdução sobre orientação à objetos.

---
+++ Treinamento/Intro_Objetos.js
@@ -0,0 +1,11 @@
+let lista_fantasma = ["Tadeu", "Judas", "Jorel", "Bichao"];
+
+console.log(lista_fantasma);
+
+lista_fantasma[2]
+
+let aluno1 = {nome: "Betina", idade: "35", curso: "T.I"}
+
+let aluno2 = {nome: "Joazin", idade: "13", curso: "T.I"}
+
+let aluno3 = {nome: "Xiquinha", idade: "75", curso: "T.I"}
