df99c9d07193b2d24beb4a40269de4d85143c003
Nodelay <121840258+Iraildo-Sousa@users.noreply.github.com>
2024-11-18 14:31:31 -0300

    Create Let_maioridade.js
    Testando let.

---
+++ Treinamento/Let_maioridade.js
@@ -0,0 +1,11 @@
+let maior_idade_legal = 18, idade = 20;
+
+let maior_idade = idade >= maior_idade_legal;
+
+console.log(maior_idade);
+
+let habilitado = true;
+
+let pode_dirigir = maior_idade && habilitado;
+
+console.log(pode_dirigir);
